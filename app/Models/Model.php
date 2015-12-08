72155ad3e1cc5e44550683d1cf7632871f5b529b
Kristian Lauttamus <kristian.lauttamus@gmail.com>
2015-12-08 23:11:25 +0200

    Changes

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -3,8 +3,45 @@
 namespace App\Models;
 
 use Illuminate\Support\Facades\DB;
+use DateTime;
+use Exception;
+use ArrayAccess;
+use Carbon\Carbon;
+use LogicException;
+use JsonSerializable;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Illuminate\Contracts\Support\Jsonable;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Routing\UrlRoutable;
+use Illuminate\Contracts\Queue\QueueableEntity;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Support\Collection as BaseCollection;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\ConnectionResolverInterface as Resolver;
+use Illuminate\Database\Eloquent\MassAssignmentException;
+use Illuminate\Database\Eloquent\Model;
 
-class Model {
+class Model Extends Model {
+    public function __construct(array $attributes = []){
+        $this->fill($attributes);
+    }
+
+    /**
+     * Get Model's table as a String
+     * @return String
+     */
     private function getTable(){
         if (isset($this->table)) {
             return $this->table;
@@ -21,5 +58,48 @@ class Model {
 
     public static function where($column, $operator, $value){
         return DB::select('select * from ' . $instance->getTable() . ' where ' . $column . ' ' . $operator .  ' ' . $value);
+    }
+
+    /**
+     * Save a new model and return the instance.
+     *
+     * @param  array  $attributes
+     * @return static
+     */
+    public static function create(array $attributes = [])
+    {
+        $model = new static($attributes);
+
+        $model->save();
+
+        return $model;
+    }
+
+    /**
+     * Fill the model with an array of attributes.
+     *
+     * @param  array  $attributes
+     * @return $this
+     *
+     * @throws \Illuminate\Database\Eloquent\MassAssignmentException
+     */
+    public function fill(array $attributes)
+    {
+        $totallyGuarded = $this->totallyGuarded();
+
+        foreach ($this->fillableFromArray($attributes) as $key => $value) {
+            $key = $this->removeTableFromKey($key);
+
+            // The developers may choose to place some attributes in the "fillable"
+            // array, which means only those attributes may be set through mass
+            // assignment to the model, and all others will just be ignored.
+            if ($this->isFillable($key)) {
+                $this->setAttribute($key, $value);
+            } elseif ($totallyGuarded) {
+                throw new MassAssignmentException($key);
+            }
+        }
+
+        return $this;
     }
 }
